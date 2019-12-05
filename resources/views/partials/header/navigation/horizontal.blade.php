9da6030a1c6edd689661a5c03bbd626a494e4033
Philipp <philipp@ouun.io>
2019-12-05 18:15:15 +0100

    Fix menu align

--- resources/views/partials/header/navigation/horizontal.blade.php
+++ resources/views/partials/header/navigation/horizontal.blade.php
@@ -1,5 +1,5 @@
 @if ($navigation)
-  <ul class="menu w-full">
+  <ul class="menu w-auto">
     @foreach ($navigation as $item)
       <li class="menu-item click-open {{ $item->classes }}  {{ 'depth-' . $loop->depth }} {{ $item->children ? 'has-children' : '' }} {{ $item->active ? 'active' : '' }}">
         <a class="relative" href="{{ $item->url }}">
