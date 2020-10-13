4b0d5572e5a88fc0c42bf2cecf6a17fbb90702a0
Daniel Sturm <d.sturm@zirkeldesign.de>
2020-10-13 17:26:03 +0200

    Soil: Activation updates for latest version

--- app/setup.php
+++ app/setup.php
@@ -49,10 +49,7 @@ add_action(
          *
          * @link https://roots.io/plugins/soil/
          */
-        add_theme_support('soil-clean-up');
-        add_theme_support('soil-nav-walker');
-        add_theme_support('soil-nice-search');
-        add_theme_support('soil-relative-urls');
+        add_theme_support('soil', array( 'clean-up', 'nav-walker', 'nice-search', 'relative-urls' ) );
 
         /**
          * Load Theme translation
